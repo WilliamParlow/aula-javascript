bfc84973ca96355a93e7c4810918ac2c2a49c8bf
WilliamParlow <william.nichelatti@catolicasc.org.br>
2018-04-06 16:41:27 -0300

    Arquivo JSON incluido

---
+++ js/buscar_veiculos.js
@@ -0,0 +1,27 @@
+// Recebe a referência do botão de busca de pacientes
+let botaoBusca = document.querySelector('#buscarVeiculo');
+
+// Adiciona o evento de clique do botão
+botaoBusca.addEventListener('click', function () {
+
+   let ajax = new XMLHttpRequest();
+
+   ajax.onreadystatechange = function () {
+
+   };
+
+   ajax.onerror = function () {
+
+   };
+
+   ajax.ontimeout = function () {
+
+   };
+
+   ajax.onabort = function () {
+
+   };
+
+   ajax.open('GET', '');
+
+});
