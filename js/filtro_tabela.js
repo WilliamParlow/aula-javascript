8ce83d3e3cdb330af7b535031f55d464f2654b0c
WilliamParlow <william.nichelatti@catolicasc.org.br>
2018-05-05 10:29:02 -0300

    Changed tbody iteration to Array.from().forEach and table reference removed

--- js/filtro_tabela.js
+++ js/filtro_tabela.js
@@ -4,31 +4,32 @@ let campoFiltro = document.querySelector('#filtrarTabela');
 // Adiciona o evento ao soltar a tecla do teclado
 campoFiltro.addEventListener('keyup', function () {
 
-   // Recebe a referência para todas as linhas do corpo da tabela
-   let linhasTabela = tabela.tBodies[0].rows;
-   // Variável que armazenara a referência de cada linha da tabela
-   let celulaMarca;
    /** Variável que recebe o valor digitado no campo de filtro 
     * a função toLowerCase() transforma todas as letras em minúsculas
-   */
+    */
    let valorFiltro = campoFiltro.value.toLowerCase();
 
-   // Estrutura de repetição que percorre cada linha da tabela
-   for (let i = 0; i < linhasTabela.length; i++) {
+   /** Estrutura de repetição que percorre cada linha da tabela
+    * Array.from() converte o vetor do tipo HTMLCollection para um vetor simples,
+    * tornando possível a iteração do vetor com forEach(). Se utilizar forEach
+    * com um vetor de HTMLCollection o console acusará um erro onde não existe
+    * a função forEach
+    */
+   Array.from(corpoTabela.rows).forEach(function (tr) {
 
       // Recebe o valor do texto da célula da tabela correspondente a marca
-      nomeMarca = linhasTabela[i].cells[0].textContent.toLowerCase();
+      nomeMarca = tr.cells[0].textContent.toLowerCase();
 
-      /* Verifica se o nome da marca da célula correspondente 
-      possui o valor do campo input de filtro */
-      if (nomeMarca.includes(valorFiltro)) {
-         // Remove a classe para tornar a linha visível
-         linhasTabela[i].classList.remove('invisivel');
-      } else {         
-         // Adiciona a classe para tornar a linha invisível
-         linhasTabela[i].classList.add('invisivel');
-      }
+         /* Verifica se o nome da marca da célula correspondente 
+         possui o valor do campo input de filtro */
+         if (nomeMarca.includes(valorFiltro)) {
+            // Remove a classe para tornar a linha visível
+            tr.classList.remove('invisivel');
+         } else {
+            // Adiciona a classe para tornar a linha invisível
+            tr.classList.add('invisivel');
+         }
 
-   }
+   });
 
 });
--- js/remover_veiculo.js
+++ js/remover_veiculo.js
@@ -1,8 +1,5 @@
-// Recebe a referência da tabela
-let tabela = document.querySelector('#tabelaVeiculos');
-
 // Adiciona o evento de clique duplo
-tabela.addEventListener('dblclick', function (event) {
+corpoTabela.addEventListener('dblclick', function (event) {
 
    /** Verifica se o alvo do clique foi uma célula da tabela 
     * Caso seja, obtemos o elemento pai desta cécula (a linha da tabela) e removemos a linha por completo
